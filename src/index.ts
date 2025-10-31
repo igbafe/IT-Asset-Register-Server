98b6c1c8c41455c6913fbe411cf8639d7e492668
peter <igbafes8@gmail.com>
2025-10-31 13:39:06 +0100

    feat:cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", // your local frontend URL
+      "https://it-asset-register-client.onrender.com", // your local frontend URL
     ],
   })
 );
